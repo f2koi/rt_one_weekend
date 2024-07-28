b3fa18387c5fddf3337f936e4631326b83d50ada
f2koi <flarefinkoi@gmail.com>
2024-07-29 02:31:11 +0900

    Add vec3 self-squaring extension

--- src/vec3_extension.rs
+++ src/vec3_extension.rs
@@ -1,10 +1,14 @@
-use gfxmath_vec3::{vec3, Vec3};
+use gfxmath_vec3::{ops::Dot, vec3, Vec3};
 
-pub trait Vec3Extension<T> {
+pub trait Vec3Extension<T>
+where
+    Vec3<T>: Dot<Output = T>,
+{
     fn unit_x() -> Vec3<T>;
     fn unit_y() -> Vec3<T>;
     fn unit_z() -> Vec3<T>;
     fn zero() -> Vec3<T>;
+    fn squared(&self) -> T;
 }
 
 impl Vec3Extension<f32> for Vec3<f32> {
@@ -22,5 +26,9 @@ impl Vec3Extension<f32> for Vec3<f32> {
 
     fn zero() -> Vec3<f32> {
         vec3!(0.0, 0.0, 0.0)
+    }
+
+    fn squared(&self) -> f32 {
+        self.dot(self)
     }
 }
