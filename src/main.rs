ab055e133c402af2d8f7bb44ecbad1e0abb00cb1
f2koi <f2koi@gmail.com>
2024-08-07 13:49:50 +0900

    Reset camera setting to that of the textbook

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,15 @@ mod vec3_extension;
 
 fn main() {
     let mut world = World::new();
-    world.add_object(Box::new(Sphere::new(vec3!(0.2, 1.0, -3.0), 0.6)));
-    world.add_object(Box::new(Sphere::new(vec3!(0.0, 0.0, -3.0), 0.5)));
-    world.add_object(Box::new(Sphere::new(vec3!(0.0, -20.0, -5.0), 19.55)));
+    world.add_object(Box::new(Sphere::new(vec3!(0.0, 0.0, -1.0), 0.5)));
+    world.add_object(Box::new(Sphere::new(vec3!(0.0, -100.5, -1.0), 100.0)));
 
     const RATIO: f32 = 16.0 / 9.0;
-    const VIEWPORT_WIDTH: f32 = 2.0;
+    const VIEWPORT_WIDTH: f32 = 3.56;
     const VIEWPORT_HEIGHT: f32 = VIEWPORT_WIDTH / RATIO;
 
     let camera_center = vec3!(0.0, 0.0, 0.0);
-    let focal_vector = vec3!(0.0, 0.0, -2.0);
+    let focal_vector = vec3!(0.0, 0.0, -1.0);
     let top_direction = Vec3::<f32>::new(0.0, 1.0, 0.0).norm().unwrap();
     let viewport = Viewport {
         width: VIEWPORT_WIDTH,
